466701062f9988d7de4d2abfb49360360e8b749a
Sean Marquez <capsulecorplab@gmail.com>
2024-03-19 20:55:15 +0000

    implement tests for project validation with full test coverage

--- packages/m30pm-lib-common/test/projects.spec.ts
+++ packages/m30pm-lib-common/test/projects.spec.ts
@@ -1,9 +1,41 @@
 import { expect } from "@oclif/test";
 import { ProjectConfiguration } from "../src/projects";
 
-describe("Project Configuration Tests", () => {
-    it("should return true for the example project", () => {
+describe("Project Validation Tests", () => {
+    it('should return true for the example project', () => {
         const project = new ProjectConfiguration("my-project", "My New m30ml Project", "CC-BY-4.0", "npm", "git", "gradle")
         expect(project.isValid()).to.equal(true)
+        project.description
+        project.license
+    })
+
+    it('should return true and have an empty description for and undefined description', () => {
+        const project = new ProjectConfiguration("my-project", undefined, "CC-BY-4.0", "npm", "git", "gradle")
+        expect(project.isValid()).to.equal(true)
+        expect(project.description).to.equal("")
+    })
+
+    it('should return false and have undefined name for invalid project name', () => {
+        const project = new ProjectConfiguration("myProject", "My New m30ml Project", "CC-BY-4.0", "npm", "git", "gradle")
+        expect(project.isValid()).to.equal(false)
+        expect(project.name).to.be.undefined
+    })
+
+    it('should return false and have undefined package manager for invalid package manager', () => {
+        const project = new ProjectConfiguration("my-project", "My New m30ml Project", "CC-BY-4.0", "not-npm", "git", "gradle")
+        expect(project.isValid()).to.equal(false)
+        expect(project.packageManager).to.be.undefined
+    })
+
+    it('should return false and have undefined version control tool for invalid version control tool', () => {
+        const project = new ProjectConfiguration("my-project", "My New m30ml Project", "CC-BY-4.0", "npm", "not-git", "gradle")
+        expect(project.isValid()).to.equal(false)
+        expect(project.versionControlTool).to.be.undefined
+    })
+
+    it('should return false and have undefined build tool for invalid build tool', () => {
+        const project = new ProjectConfiguration("my-project", "My New m30ml Project", "CC-BY-4.0", "npm", "git", "not-gradle")
+        expect(project.isValid()).to.equal(false)
+        expect(project.buildTool).to.be.undefined
     })
 })
