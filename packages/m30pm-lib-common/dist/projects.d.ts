1792cd9d56e1ac849ec6896fbfd0cd09ae987275
Sean Marquez <capsulecorplab@gmail.com>
2024-03-22 20:29:37 +0000

    rerun `npm run build` in m30pm-lib-common and rerun `npm i` in project root

--- packages/m30pm-lib-common/dist/projects.d.ts
+++ packages/m30pm-lib-common/dist/projects.d.ts
@@ -1,16 +1,21 @@
 import * as Enums from './enums';
+export declare const DefaultVersion = "0.0.0";
 export declare const DefaultLicense = "CC0-1.0";
 export declare class ProjectConfiguration {
     private _name;
+    private _version;
     private _description;
+    private _author;
     private _license;
     private _packageManager;
     private _versionControlTool;
     private _buildTool;
-    constructor(name: string, description: string | undefined, license: string, packageManager: string, versionControlTool: string, buildTool: string);
+    constructor(name: string, version: string, description: string | undefined, author: string | undefined, license: string, packageManager: string, versionControlTool: string, buildTool: string);
     isValid(): boolean;
     get name(): string | undefined;
+    get version(): string | undefined;
     get description(): string;
+    get author(): string;
     get license(): string;
     get packageManager(): Enums.PackageManagers | undefined;
     get versionControlTool(): Enums.VersionControlTools | undefined;
--- packages/m30pm-lib-common/dist/projects.js
+++ packages/m30pm-lib-common/dist/projects.js
@@ -22,36 +22,53 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ProjectConfiguration = exports.DefaultLicense = void 0;
+exports.ProjectConfiguration = exports.DefaultLicense = exports.DefaultVersion = void 0;
 const Enums = __importStar(require("./enums"));
+const semver_1 = __importDefault(require("semver"));
+exports.DefaultVersion = '0.0.0';
 exports.DefaultLicense = 'CC0-1.0';
 class ProjectConfiguration {
     _name;
+    _version;
     _description;
+    _author;
     _license;
     _packageManager;
     _versionControlTool;
     _buildTool;
-    constructor(name, description, license, packageManager, versionControlTool, buildTool) {
+    constructor(name, version, description, author, license, packageManager, versionControlTool, buildTool) {
         let npmPackageNameRegEx = new RegExp('^(@[a-z0-9-~][a-z0-9-._~]*\/)?[a-z0-9-~][a-z0-9-._~]*$');
         if (npmPackageNameRegEx.test(name)) {
             this._name = name;
         }
+        if (semver_1.default.valid(version)) {
+            this._version = version;
+        }
         this._description = description ? description : "";
+        this._author = author ? author : "";
         this._license = license;
         this._packageManager = Enums.stringToEnumValue(Enums.PackageManagers, packageManager);
         this._versionControlTool = Enums.stringToEnumValue(Enums.VersionControlTools, versionControlTool);
         this._buildTool = Enums.stringToEnumValue(Enums.BuildTools, buildTool);
     }
     isValid() {
-        return this._name !== undefined && this._license !== '' && this._packageManager !== undefined && this._versionControlTool !== undefined && this._buildTool !== undefined;
+        return this._name !== undefined && this._version !== undefined && this._license !== '' && this._packageManager !== undefined && this._versionControlTool !== undefined && this._buildTool !== undefined;
     }
     get name() {
         return this._name;
     }
+    get version() {
+        return this._version;
+    }
     get description() {
         return this._description;
+    }
+    get author() {
+        return this._author;
     }
     get license() {
         return this._license;
