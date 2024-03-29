52d339b34359f6f351b07c1ecd10f2cece5d0eaa
Sean Marquez <capsulecorplab@gmail.com>
2024-03-29 21:08:54 +0000

    implent remaining tests for ProjectConfiguration.fromJsonString()

--- packages/m30pm-lib-common/test/projects.spec.ts
+++ packages/m30pm-lib-common/test/projects.spec.ts
@@ -72,7 +72,7 @@ describe("To JSON-string Tests", () => {
     })
 })
 
-describe("Project Validation Tests (json-based constructor)", () => {
+describe("Project Validation Tests for ProjectConfiguration.fromJsonString()", () => {
     it('should return true for the example project from package.json string', () => {
         const project = new ProjectConfiguration("my-project", "0.0.0", "My New m30ml Project", "Mach 30", "CC-BY-4.0", "npm", "git", "gradle")
         const project2 = ProjectConfiguration.fromJsonString(project.getJsonString())
@@ -80,6 +80,64 @@ describe("Project Validation Tests (json-based constructor)", () => {
         project2.description
         project2.author
         project2.license
+    })
+
+    it('should return true for the example project but with yarn from package.json string', () => {
+        const project = new ProjectConfiguration("my-project", "0.0.0", "My New m30ml Project", "Mach 30", "CC-BY-4.0", "yarn", "git", "gradle")
+        const project2 = ProjectConfiguration.fromJsonString(project.getJsonString())
+        expect(project2.isValid()).to.equal(true)
+        project2.description
+        project2.author
+        project2.license
+    })
+
+    it('should return false and have an empty name for invalid project name from package.json string', () => {
+        const project = new ProjectConfiguration("myProject", "0.0.0", "My New m30ml Project", "Mach 30", "CC-BY-4.0", "npm", "git", "gradle")
+        const project2 = ProjectConfiguration.fromJsonString(project.getJsonString())
+        expect(project2.isValid()).to.equal(false)
+        expect(project2.name).to.equal("")
+    })
+
+    it('should return false and have an empty version for invalid version from package.json string', () => {
+        const project = new ProjectConfiguration("my-project", "bad version", "My New m30ml Project", "Mach 30", "CC-BY-4.0", "npm", "git", "gradle")
+        const project2 = ProjectConfiguration.fromJsonString(project.getJsonString())
+        expect(project2.isValid()).to.equal(false)
+        expect(project2.version).to.equal("")
+    })
+
+    it('should return true and have an empty description for and undefined description from package.json string', () => {
+        const project = new ProjectConfiguration("my-project", "0.0.0", undefined, "Mach 30", "CC-BY-4.0", "npm", "git", "gradle")
+        const project2 = ProjectConfiguration.fromJsonString(project.getJsonString())
+        expect(project2.isValid()).to.equal(true)
+        expect(project2.description).to.equal("")
+    })
+
+    it('should return true and have an empty author for and undefined author from package.json string', () => {
+        const project = new ProjectConfiguration("my-project", "0.0.0", "My New m30ml Project", undefined, "CC-BY-4.0", "npm", "git", "gradle")
+        const project2 = ProjectConfiguration.fromJsonString(project.getJsonString())
+        expect(project2.isValid()).to.equal(true)
+        expect(project2.author).to.equal("")
+    })
+
+    it('should return false and have invalid package manager for invalid package manager from package.json string', () => {
+        const project = new ProjectConfiguration("my-project", "0.0.0", "My New m30ml Project", "Mach 30", "CC-BY-4.0", "not-npm", "git", "gradle")
+        const project2 = ProjectConfiguration.fromJsonString(project.getJsonString())
+        expect(project2.isValid()).to.equal(false)
+        expect(project2.packageManager).to.equal(PackageManagers.INVALID_PM)
+    })
+
+    it('should return false and have invalid version control tool for invalid version control tool from package.json string', () => {
+        const project = new ProjectConfiguration("my-project", "0.0.0", "My New m30ml Project", "Mach 30", "CC-BY-4.0", "npm", "not-git", "gradle")
+        const project2 = ProjectConfiguration.fromJsonString(project.getJsonString())
+        expect(project2.isValid()).to.equal(false)
+        expect(project2.versionControlTool).to.equal(VersionControlTools.INVALID_VCT)
+    })
+
+    it('should return false and have invalid build tool for invalid build tool from package.json string', () => {
+        const project = new ProjectConfiguration("my-project", "0.0.0", "My New m30ml Project", "Mach 30", "CC-BY-4.0", "npm", "git", "not-gradle")
+        const project2 = ProjectConfiguration.fromJsonString(project.getJsonString())
+        expect(project2.isValid()).to.equal(false)
+        expect(project2.buildTool).to.equal(BuildTools.INVALID_BT)
     })
 
     it('should return false for malformed package.json string', () => {
