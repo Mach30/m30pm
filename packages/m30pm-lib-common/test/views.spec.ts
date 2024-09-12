f2030df7917d7785ff87346f3a92e4675bae8a63
Sean Marquez <capsulecorplab@gmail.com>
2024-09-12 21:16:53 +0000

    implement info-noError md log view definition

--- packages/m30pm-lib-common/test/views.spec.ts
+++ packages/m30pm-lib-common/test/views.spec.ts
@@ -76,4 +76,13 @@ describe("CommandHistoryLog View Tests", () => {
         let expectedView = fs.readFileSync(path.resolve(__dirname, "../src/resources/spec/cmd-history-log/createProject-debug-noError.md"), "utf8").toString();
         expect(view).to.equal(expectedView);
     })
+    it('should return createProject-info-noError.md for createProject-info-noError.yaml', () => {
+        let specObjectYaml = fs.readFileSync(path.resolve(__dirname, "../src/resources/spec/cmd-history-log/createProject-info-noError.yaml"), "utf8").toString();
+        let specObject = yaml.load(specObjectYaml) as Object;
+        let viewContext : any = {};
+        viewContext["data"] = specObject;
+        let view = ViewRenderer.render(BuiltinViews.getCommandHistoryLogMdView(), viewContext);
+        let expectedView = fs.readFileSync(path.resolve(__dirname, "../src/resources/spec/cmd-history-log/createProject-info-noError.md"), "utf8").toString();
+        expect(view).to.equal(expectedView);
+   })
 })
